9a440aa71b04d8873942a0e5bb2bbf8cc12b2d6d
Gabriel Rodrigues <birodrigs@gmail.com>
2026-01-13 20:41:43 -0300

    Add OAuth2 support and update dependencies
    Added django-oauth-toolkit and related dependencies to requirements.txt for OAuth2 support. Updated settings.py to include 'oauth2_provider' in INSTALLED_APPS and load environment variables using dotenv.

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+load_dotenv()
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'taskio'
+    'taskio',
+    'oauth2_provider',
 ]
 
 MIDDLEWARE = [
