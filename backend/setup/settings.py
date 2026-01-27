befa1154a0e21a5ce1841cb39ee75770016b96fa
Gabriel Rodrigues <birodrigs@gmail.com>
2026-01-26 21:00:11 -0300

    Add CORS support with django-cors-headers
    Integrated 'corsheaders' into INSTALLED_APPS and middleware to enable CORS support. Configured allowed origins and credentials for local development and specific IP addresses.

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -49,10 +49,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taskio',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,4 +142,15 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "http://192.168.1.19:3000"
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.1.19:3000"
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOW_CREDENTIALS = True
